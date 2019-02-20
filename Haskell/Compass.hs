66ad46d8c176a79e56f06a847b5e8bcb927bacfb
John Lusk <john-github@how-hard-can-it-be.com>
2019-02-19 21:55:18 -0500

    pattern matching and data type constructors

---
+++ Haskell/Compass.hs
@@ -0,0 +1,30 @@
+module Compass where
+
+data Direction = North | East | South | West deriving (Eq, Show)
+
+bearing :: Direction -> Int
+bearing dir = if dir == North
+              then 0
+              else if dir == East
+                   then 90
+                   else if dir == South
+                        then 180
+                        else 270
+
+bearingCase :: Direction -> Int
+bearingCase dir = case dir of
+  North -> 0
+  East  -> 90
+  South -> 180
+  West  -> 270
+
+bearingPat :: Direction -> Int
+bearingPat North  = 0
+bearingPat East   = 90
+bearingPat South  = 180
+bearingPat West   = 270
+
+-- divAbyB :: (Eq t, Fractional t) => t -> t -> Maybe t
+divAbyB a 0 = Nothing
+divAbyB a b = Just (a/b)
+
---
+++ Haskell/CompassSpec.hs
@@ -0,0 +1,36 @@
+module CompassSpec where
+
+import Test.Hspec
+
+import Compass
+
+main :: IO ()
+main = hspec $ do
+  describe "bearing" $ do
+    it "North is 0" $ do
+      bearing North == 0
+    it "East is 90" $ do
+      bearing East == 90
+    it "South is 180" $ do
+      bearing South == 180
+    it "West is 270" $ do
+      bearing West == 270
+  describe "bearingCase" $ do
+    it "North is 0" $ do
+      bearingCase North == 0
+    it "East is 90" $ do
+      bearingCase East == 90
+    it "South is 180" $ do
+      bearingCase South == 180
+    it "West is 270" $ do
+      bearingCase West == 270
+  describe "bearingPat" $ do
+    it "North is 0" $ do
+      bearingPat North == 0
+    it "East is 90" $ do
+      bearingPat East == 90
+    it "South is 180" $ do
+      bearingPat South == 180
+    it "West is 270" $ do
+      bearingPat West == 270
+  
